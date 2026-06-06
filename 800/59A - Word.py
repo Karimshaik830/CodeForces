569a00c2d7d5a5e48e45ba12c9c8fcbe84bd3a37
Karimshaik830 <mmasthan338@gmail.com>
2026-06-06 23:01:45 +0530

    06/06/2026

---
+++ 800/59A - Word.py
@@ -0,0 +1,18 @@
+def solve():
+    s = input()
+    upper = 0
+    lower = 0
+    for char in s:
+        if char.isupper():
+            upper += 1
+        else:
+            lower += 1
+
+    if upper > lower:
+        print(s.upper())
+    else:
+        print(s.lower())
+
+
+if __name__ == '__main__':
+    solve()
