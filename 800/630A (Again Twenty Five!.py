9186bb1a066db7ca99236243546d74b9af8dc943
Karimshaik830 <mmasthan338@gmail.com>
2026-07-21 10:36:46 +0530

    21/07/2026

---
+++ 800/630A (Again Twenty Five!.py
@@ -0,0 +1,10 @@
+def solve():
+    # Read the input n (even though we don't need to use it)
+    n = input()
+
+    # The last two digits of 5^n (for n >= 2) are always 25
+    print("25")
+
+
+if __name__ == '__main__':
+    solve()
